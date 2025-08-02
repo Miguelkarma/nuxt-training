1224213df090af1a7aafa46b9c498d317337fca2
Miguelkarma <santos.p.miguel46@gmail.com>
2025-08-02 20:56:31 +0800

    routing Pages

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
