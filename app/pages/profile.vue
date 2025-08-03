062b217aa90156d0ddba05f51a3f8aa6466b0070
Miguelkarma <santos.p.miguel46@gmail.com>
2025-08-03 14:09:13 +0800

    using composables (useUtils) also can use the vueUse opensource library for fast dev

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -4,6 +4,6 @@ sayHello();
 </script>
 <template>
   <div class="container py-3 mx-auto Main">
-    <h1 class="text-teal-500">Hello Nuxt 3</h1>
+    <h1 class="text-teal-500">profile</h1>
   </div>
 </template>
